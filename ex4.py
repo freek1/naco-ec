b3702a7a0400e98276f9193e5d94a83f273bf9ae
gklein <gklein@science.ru.nl>
2023-03-11 23:53:21 +0100

    complete rework of assignment 4

--- ex4.py
+++ ex4.py
@@ -10,14 +10,14 @@ goal_string = 'group two cool'
 K = 2 # Tournament parameter
 mu = 0.01 # Mutation rate
 
-l = 14 # Length of strings
+l = len(goal_string) # Length of strings
 alphabet = 'abcdefghijklmnopqrstuvwxyz '
 N = 1000 # Population size
 gens = 1500 # Generations
 p_cross = 1 # Crossover prob
 
 prev_fit = 0
-fittest = np.zeros(gens)
+fittest = []
 
 # Defining fitness score (closeness to goal sequence)
 def fitness(s, goal_string):
@@ -27,14 +27,15 @@ def fitness(s, goal_string):
     bools = [[*s][i] == [*goal_string][i] for i in range(len(goal_string))]
     return np.sum(bools*1)
 
+# apply 1 cut crossover between two strings
 def crossover(p1, p2):
     '''Applies crossover permutation
     Input:
-        p1: parent candidate list (length len(string))
-        p2: parent candidate list (length len(string))
+        p1: parent
+        p2: parent
     Output:
-        c1: children, mutated candidate lists (length len(string))
-        c2: children, mutated candidate lists (length len(string))
+        c1: children
+        c2: children
     '''
     p1 = [*p1]
     p2 = [*p2]
@@ -42,99 +43,88 @@ def crossover(p1, p2):
     c1 = np.empty_like(p1)
     c2 = np.empty_like(p2)
     # Choose two cut points
-    cut1 = np.random.randint(1, len(p1)-1)
-    cut2 = np.random.randint(cut1, len(p1))
+    cut = np.random.randint(1, len(p1)-1)
     # Keep middle piece
-    c1[cut1:cut2] = p1[cut1:cut2]
-    c2[cut1:cut2] = p2[cut1:cut2]
-    # Take complement in other parent
-    o1 = []
-    o2 = []
-    
-    for i in range(len(p1)):
-        if p1[i] not in c2[cut1:cut2]:
-            o2.append(p1[i])
-        if p2[i] not in c1[cut1:cut2]:
-            o1.append(p2[i])
-    
-    # Fill gaps in order (starting after 2nd cut)
-    if len(o1) == (cut1+len(c1)-cut2) and len(o2) > (cut1+len(c1)-cut2):
-        for i in range(cut2, cut1+len(c1)):
-            c1[i%len(c1)] = o1.pop(0)
-        for i in range(cut2, cut1+len(c1)):
-            c2[i%len(c1)] = o2.pop(0)
-    else:
-        for i in range(cut2, cut1+len(c1)):
-            c1[i%len(c1)] = ' '
-        for i in range(cut2, cut1+len(c1)):
-            c2[i%len(c1)] = ' '
+    c1 = p2[0:cut]+p1[cut:len(p1)]
+    c2 = p1[0:cut]+p2[cut:len(p1)]
+
     return c1, c2
 
-def select_tournament(group):
-    ''' Tournament selection from tournament 'group' '''
-    fs = 0
-    parent = -1
-    for p in group:
-        if fit_scores[p] > fs:
-            parent = p
-    return parent
+# tournement selection over the population
+def tournament_selection(fit_scores, K):
+    tour_population_idx = random.sample(range(len(fit_scores)), K)
+    best_participant = -1
+    best_fitness = -1
+    for participant_idx in tour_population_idx:
+        fitness_participant = fit_scores[participant_idx]
+        if best_fitness < fitness_participant:
+            best_fitness = fitness_participant
+            best_participant = participant_idx
+    parent_idx = best_participant
+    return parent_idx
 
-# Create N random strings
-strings = [''.join(random.choices(alphabet, k=l)) for s in range(N)]
+# position-wise random character with rate mu
+def mutation(canidate, mu, alphabet):
+    if np.random.rand(1) < mu: # if mutation happens
+        switch = np.random.randint(0, len(canidate))
+        mutated_char = random.choice(alphabet)
+        canidate[switch] = mutated_char
+        return ''.join(canidate)
+    else:
+        return ''.join(canidate)
+    
 
-for gen in tqdm(range(gens)):
-    # Compute fitness
-    fit_scores = np.zeros((N))
-    for i, s in enumerate(strings):
-        fit_scores[i] = fitness(s, goal_string)
+def GA(alphabet, K, mu, goal_string, gens, n_runs, N, l, p_coss):
+    avg_runs_duration = []
+    for run in tqdm(range(n_runs)):
+        prev_fit = 0
+        fittest = []
+        fit_scores = np.zeros((N))
+        population = [''.join(random.choices(alphabet, k=l)) for s in range(N)]
+        for gen in range(gens):
+            # fitness calculation
+            fit_scores = np.zeros((N))
+            for i, s in enumerate(population):
+                fit_scores[i] = fitness(s, goal_string)
+            
+            fittest.append(np.max(fit_scores))
 
-    # Select K indiv.
-    parents = np.random.choice(len(fit_scores), size=K)
-    # Select fittest 2
-    p1 = select_tournament(parents)
+            if np.max(fit_scores) == l:
+                avg_runs_duration.append(gen)
+                break
 
-    # Select K indiv.
-    parents = np.random.choice(len(fit_scores), size=K)
-    # Select fittest 2
-    p2 = select_tournament(parents)
+            new_population = []
+            for n in range(0, N, 2):
+                # tournament selection
+                parent1_idx = tournament_selection(fit_scores, K)
+                parent2_idx = tournament_selection(fit_scores, K)
 
-    # Ensure parents are different
-    while (p1 == p2):  
-        parents = np.random.choice(len(fit_scores), size=K)
-        p2 = select_tournament(parents)
-        
-    # Crossover
-    if np.random.rand() < p_cross:
-        c1, c2 = crossover(strings[parents[0]], strings[parents[1]])
+                parent1 = population[parent1_idx]
+                parent2 = population[parent2_idx]
 
-    # Mutation
-    if np.random.rand(1) < mu:
-        switch = np.random.randint(0, len(c1), 2)
-        while(switch[0] == switch[1]):
-            switch = np.random.randint(0, len(c1), 2)
-        c1[[switch[0], switch[1]]] = c1[[switch[1], switch[0]]]
-    if np.random.rand(1) < mu:
-        switch = np.random.randint(0, len(c1), 2)
-        while(switch[0] == switch[1]):
-            switch = np.random.randint(0, len(c1), 2)
-        c2[[switch[0], switch[1]]] = c2[[switch[1], switch[0]]]
+                # Crossover
+                if np.random.rand() < p_cross:
+                    c1, c2 = crossover(parent1, parent2)
 
-    # Replace parents with better children (if better)
-    fitc1 = fitness(c1, goal_string)
-    fitc2 = fitness(c2, goal_string)
-    if fitc1 > fit_scores[parents[0]]:
-        strings[parents[0]] = c1
-        fit_scores[parents[0]] = fitc1
-    if fitc2 > fit_scores[parents[1]]:
-        strings[parents[1]] = c2
-        fit_scores[parents[1]] = fitc2
+                # Mutation
+                c1 = mutation(c1, mu, alphabet)
+                c2 = mutation(c2, mu, alphabet)
+                new_population.append(c1)
+                new_population.append(c2)
+            
+            population = new_population
 
-    # Store better max
-    if max(fit_scores) > prev_fit:
-        prev_fit = max(fit_scores)
-        
-    fittest[gen] = prev_fit
+        plt.plot(np.arange(gen+1), fittest, '--', label=f'run {run+1}')
+    plt.xlabel('Generations')
+    plt.ylabel('Fitness')
+    plt.title('String search GA')
+    plt.show()
+    return np.mean(avg_runs_duration)
 
+n_runs = 5
+avg = GA(alphabet, K, mu, goal_string, gens, n_runs, N, l, p_cross)
+print(avg)
+#print(f"number of generations until finding of target string: {gen}")
 plt.plot(np.arange(gens), fittest)
 plt.xlabel('Generations')
 plt.ylabel('Fitness')
